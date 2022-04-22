6bb698321b307aeb8926ed41b6fc3d069c36ad7b
Mnemo128 <mnemostereotyp@outlook.com>
2022-04-22 09:43:03 -0400

    Weiteres Ausgestalten der Optik

--- personen.py
+++ personen.py
@@ -12,46 +12,50 @@ print(' Mithilfe dieser Software können Sie Personen mit Vor- und Nachnamen erf
 
 wahl=''
 while (wahl != 'Q'):
-    wahl=input('Wählen Sie einen der folgenden Menuepunkte: \n \n -Person erfassen (A)\n -Personen anzeigen (B)\n -Beenden (Q) \n\n\n\n\n\n\n\n\n\n\n\n\nAuswahl:   ').upper()
-    while wahl !='A'and wahl !='B':
+    wahl=input('Wählen Sie einen der folgenden Menuepunkte: \n \n -Person erfassen  -> Druecken Sie:  A \n -Liste anzeigen   -> Druecken Sie:  B \n -Beenden          -> Druecken Sie:  Q  \n\n\n\n\n\n\n\n\n\n\n\n\nAuswahl:   ').upper()
+    while wahl !='A'and wahl !='B' and wahl!='Q':
         print(5*'\n',31*'_','Warnung',40*'_',5*'\n','\nUngueltige Eingabe.\n',5*'\n',80*'_',5*'\n')
         break
 
 
-#####Erfasst Person
+#####Erfasst Person und zeigt Ergebnis
 
 
     if wahl =='A':
-        print(15*'\n',80*'_','\n','Um eine Person zu erfassen benoetigt man Vor- und Nachnamen.','\n',80*'_',25*'\n')
+        print(19*'\n',80*'_','\n','Um eine Person zu erfassen benoetigt man Vor- und Nachnamen.','\n',80*'_',25*'\n')
         persons = []
         Vorname = input('Bitte geben Sie zunaechst einen Vornamen ein:  ')
         Nachname = input('Bitte geben Sie nun einen Nachnamen ein:  ')
         person ={'Vorname': Vorname,'Nachname': Nachname }
         persons.append(person)
-        print(f'\nDie Person: {Vorname} {Nachname} wurde, wie folgt angelegt:\n')
-
-
-####Schreibt Person in Txt Datei
-
-
+        print(20*'\n')
+        print(f'\nDie Person:" {Vorname} {Nachname} " wurde, wie folgt angelegt:\n\n\n')
         for person in persons:
+            print(80*'_','\n')
             print(f'Vorname: {person["Vorname"]}')
             print(f'Nachname: {person["Nachname"]}')
+
+
+####In Txt Datei schreiben
+
+
         with open('Personen.txt', 'a') as file_handle:
             file_handle.write(str(persons))
-            print('\nPerson wurde in Liste übertragen.\n')
-
+            print(80*'_')
+            print('\nSchreibe....\n\n')
+            print('...Person wurde in Liste ("Personen.txt") übertragen!\n')
+            print(80*'_')
 
 ######Zeigt Inhalt der abgespeicherten Liste an
 
 
     if wahl =='B':
-        print(20*'\n')
+        print(10*'\n',80*'_','\n','Die Liste einthält folgende Daten: ')
         with open('Personen.txt', 'r')as file_handle:
-            line= file_handle.read().split('{')
+            line= file_handle.read().split('[')
             for person in line:
                 print(person)
-
+        print(2*'\n',80*'_',10*'\n')
 
 
 
@@ -59,6 +63,7 @@ while (wahl != 'Q'):
 
 
 
-print('\nDas Programm wurde beendet.\nAuf Wiedersehen. \n')
+print(20*'\n','- P E R S O N E N - E R F A S S U N G S - S O F T W A R E - \n -MarkusMichaelLedwig\n',80*'_')
+print(10*'\n','Das Programm wurde beendet.\nAuf Wiedersehen.')
 
-print(42*'\n')
+print(22*'\n')
